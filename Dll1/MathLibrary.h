562aff3f8c81ae4305cc851d998a81a33d5fe25f
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-07-29 07:34:39 +0800

    unicode naming for class; leave one API, enough for demo

--- Dll1/MathLibrary.h
+++ Dll1/MathLibrary.h
@@ -9,14 +9,9 @@
 namespace 数学库
 {
 	// This class is exported from the MathLibrary.dll  
-	class Functions
+	class 函数
 	{
 	public:
 		static 数学库_API double 加(double a, double b);
-		/*
-		static MATHLIBRARY_API double 乘(double a, double b);
-
-		// Returns a + (a * b)  
-		static MATHLIBRARY_API double 加乘(double a, double b);*/
 	};
 }
